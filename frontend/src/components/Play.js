4c00d46c964f757fe53f15011b08a7d95b46b005
hardkov <krzysztofH150@interia.pl>
2021-02-08 01:38:30 +0100

    [frontend] added chat

--- frontend/src/components/Play.js
+++ frontend/src/components/Play.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Chessboard from "chessboardjsx";
 import Grid from '@material-ui/core/Grid';
+import GameChat from "./GameChat.js";
 
 export default function Play(){
     return (
         <div style={{ padding: "10vw"}}>
             <Grid container direction="row" justify="center" alignItems="center">
-                <Grid item>
-                    {}
+                <Grid item xs={3}>
+                    <GameChat />
                 </Grid>
-                <Grid item >
+                <Grid xs={6}>
                     <Chessboard position="start" />
                 </Grid>
-                <Grid item>
-                    {}
+                <Grid item xs={3}>
+                    <GameChat />
                 </Grid>
             </Grid>   
         </div>
